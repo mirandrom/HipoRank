275d1d1061a63c99c925e9fca2d113b73aee5cfd
amr-amr <andrei.romascanu@mail.mcgill.ca>
2020-01-28 15:13:34 -0500

    feat: cuda and from_pretrained

--- hipo_rank/embedders/bert.py
+++ hipo_rank/embedders/bert.py
@@ -11,8 +11,10 @@ class BertEmbedder:
     def __init__(self, bert_config_path: str, bert_model_path: str,
                  bert_tokenizer: str = "bert-base-cased",
                  bert_pretrained: str = None,
-                 max_seq_len: int = 60):
+                 max_seq_len: int = 60,
+                 cuda: bool = True):
         self.max_seq_len = max_seq_len
+        self.cuda = cuda
         if bert_pretrained:
             self.bert_model = BertModel.from_pretrained(bert_pretrained)
             self.bert_tokenizer = BertTokenizer.from_pretrained(bert_pretrained)
@@ -23,7 +25,10 @@ class BertEmbedder:
     def _load_bert(self, bert_config_path: str, bert_model_path: str):
         bert_config = BertConfig.from_json_file(bert_config_path)
         model = BertModel(bert_config)
-        model_states = torch.load(bert_model_path, map_location='cpu')
+        if self.cuda:
+            model_states = torch.load(bert_model_path)
+        else:
+            model_states = torch.load(bert_model_path, map_location='cpu')
         # fix model_states
         for k in list(model_states.keys()):
             if k.startswith("bert."):
@@ -32,6 +37,8 @@ class BertEmbedder:
                 _ = model_states.pop(k)
 
         model.load_state_dict(model_states)
+        if self.cuda:
+            model.cuda()
         model.eval()
         return model
 
@@ -39,16 +46,30 @@ class BertEmbedder:
         # TODO: clean up batch approach
         input_ids = [self.bert_tokenizer.encode(s, add_special_tokens=True) for s in sentences]
         padded_len = min(max([len(x) for x in input_ids]), self.max_seq_len)
-        batch_size = len(input_ids)
-        input_tensor = np.zeros((batch_size, padded_len))
+        num_inputs = len(input_ids)
+        input_tensor = np.zeros((num_inputs, padded_len))
         for i,x in enumerate(input_ids):
-            input_tensor[i][:padded_len] = x[:padded_len]
-        input_tensor = torch.LongTensor(input_tensor)
-        # Original pacsum paper uses [CLS] next sentence prediction activations
-        # this isn't optimal and should be changed for potentially better performance
-        with torch.no_grad():
-             pooled_output = self.bert_model(input_tensor)[1].numpy() # shape = (x, 768)
-        return pooled_output
+            l = min(padded_len, len(x))
+            input_tensor[i][:l] = x[:l]
+        if self.cuda:
+            input_tensor = torch.LongTensor(input_tensor).to('cuda')
+        else:
+            input_tensor = torch.LongTensor(input_tensor)
+        batch_size = 20
+        pooled_outputs = []
+        for i in range(0, num_inputs, batch_size):
+            input_batch = input_tensor[i:i+batch_size]
+            # Original pacsum paper uses [CLS] next sentence prediction activations
+            # this isn't optimal and should be changed for potentially better performance
+            with torch.no_grad():
+                pooled_output = self.bert_model(input_batch)[1] # shape = (x, 768)
+            if self.cuda:
+                pooled_output = pooled_output.cpu()
+            else:
+                pooled_output = pooled_output
+            pooled_outputs.append(pooled_output)
+        pooled_outputs = torch.cat(pooled_outputs).numpy()
+        return pooled_outputs
 
     def get_embeddings(self, doc: Document) -> Embeddings:
         sentence_embeddings = []
