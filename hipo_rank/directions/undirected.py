fe4a1a50bdf7c87e118ac8a477c048221ef04c84
amr-amr <andrei.romascanu@mail.mcgill.ca>
2020-01-28 19:07:44 -0500

    fix: create undirected edges consistently

--- hipo_rank/directions/undirected.py
+++ hipo_rank/directions/undirected.py
@@ -3,6 +3,18 @@ from hipo_rank import Similarities
 
 class Undirected:
     def update_directions(self, similarities: Similarities) -> Similarities:
+        similarities.sect_to_sect.directions = ["undirected" for _ in similarities.sect_to_sect.directions]
+
+        for sect1_index, sent_sims in enumerate(similarities.sent_to_sect):
+            similarities.sent_to_sect[sect1_index].directions = [
+                "undirected" for _ in  similarities.sent_to_sect[sect1_index].directions
+            ]
+
+        for i, sent_sims in enumerate(similarities.sent_to_sent):
+            similarities.sent_to_sent[i].directions = [
+                "undirected" for _ in
+                similarities.sent_to_sent[i].directions
+            ]
         return similarities
 
 
